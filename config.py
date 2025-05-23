97ca22a6c875d6c2a95e097042deadd73daba4f8
DAVAAFDIANSYAH <dava21947@gmail.com>
2025-05-24 01:56:18 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+SECRET_KEY = "super-secret-key"  
+JWT_SECRET_KEY = "rezaanakpunk"  
+API_KEYS = {"1234567890abcdef"}  
+MONGO_URI = "mongodb+srv://auth_db:dapa1234@cluster0.rvtlsdp.mongodb.net"
+DB_NAME = "auth_db"
+SENDER_EMAIL = "davaafdiansyah58@gmail.com"        # Ganti dengan email Gmail yang akan dipakai kirim OTP
+SENDER_PASSWORD = "emtr djsa ottd pwbl"            # Ganti dengan App Password Gmail (bukan password akun biasa)
